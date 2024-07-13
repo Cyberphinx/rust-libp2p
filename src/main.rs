db818ef256b9d0074b761ed3f2d8563131d1d969
Cyberphinx <88593809+Cyberphinx@users.noreply.github.com>
2024-07-13 18:00:21 +0100

    feat: tcp tls yamux transport

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .with_env_filter(EnvFilter::from_default_env())
         .init();
 
-    // Identities in libp2p are handled via a public/private key pair,
-    // Nodes identify each other via their PeerId which is derived from their public key
-    let mut swarm = libp2p::SwarmBuilder::with_new_identity();
+    let mut swarm = libp2p::SwarmBuilder::with_new_identity()
+        .with_async_std()
+        .with_tcp(
+            libp2p::tcp::Config::default(),
+            libp2p::tls::Config::new,
+            libp2p::yamux::Config::default,
+        )?;
 
     Ok(())
 }
